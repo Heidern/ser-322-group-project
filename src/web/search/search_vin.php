e0c72009fbc2c02783473926876aafda22435e3f
drmorri8 <drmorri8@asu.edu>
2017-02-28 21:17:22 -0800

    Update search_vin.php

--- src/web/search/search_vin.php
+++ src/web/search/search_vin.php
@@ -81,6 +81,6 @@
             }
         ?>
     </fieldset>
-    <p><font size = "2" ><i>Searches for a car with a specific VIN in and determines make and model.</i></p>
+    <p><font size = "2" ><i>Searches for a car with a specific VIN in and determines make and model. (Try 12345678901234568)</i></p>
 </body>
 </html>
