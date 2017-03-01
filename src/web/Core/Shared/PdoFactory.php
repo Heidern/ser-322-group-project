4221ed0d3c05486b2f882644625721d1be5dc31f
drmorri8 <drmorri8@asu.edu>
2017-02-28 21:06:49 -0800

    Made PdoFactory generic

--- src/web/Core/Shared/PdoFactory.php
+++ src/web/Core/Shared/PdoFactory.php
@@ -1,13 +1,9 @@
 <?php
-
 namespace Core\Shared;
-
 use \PDO;
-
 class PdoFactory {
     public static function getPdoObject () {
-        return new PDO('mysql:host=192.168.1.200:32768;dbname=car_dealer;charset=utf8mb4', 'admin', 'password');
+        return new PDO('mysql:host=localhost;dbname=car_dealer;charset=utf8mb4', 'root', '');
     }
 }
-
 ?>
